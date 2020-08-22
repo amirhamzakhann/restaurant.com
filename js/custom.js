3daa9803aedf665cb82221bebad48d5c1c3a80ea
amirhamzakhan <shuvokhan2044@gmail.com>
2020-08-22 15:23:54 +0600

    restaurant

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// Custom Function Start Here
+
+$(function () {
+
+    //  Banner-Slider Jquery Code Start Here
+    $('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        spped: 300,
+      });
+    //  Banner-Slider Jquery Code End Here
+
+    //  Common Jquery Code Start Here
+
+    //  Common Jquery Code End Here
+
+});
+// Custom Function End Here
